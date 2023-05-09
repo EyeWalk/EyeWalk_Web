e3904f67a4594ed6adcfcc307f7bad3b8fe2f5fb
Ellen Gomes <ellen_gomes14@hotmail.com>
2023-05-08 23:10:31 -0300

    creating front page structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useRef } from 'react';
 import './App.css';
+import ScrollTop from './components/ScrollToTopButton';
+import Toolbar from '@mui/material/Toolbar';
+import * as React from 'react';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer/Footer';
 
 function App() {
+  const inicio = useRef(null);
+  const quemSomos = useRef(null);
+  const eyewalk = useRef(null);
+  const planos = useRef(null);
+
+  const scrollTo = (elementRef) => {
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: "smooth"
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar>
+        <button className='navLink' onClick={() => scrollTo(inicio)}>Início</button>
+        <button className='navLink' onClick={() => scrollTo(quemSomos)}>Quem Somos</button>
+        <button className='navLink' onClick={() => scrollTo(planos)}>Planos</button>
+        <button className='navLink' onClick={() => scrollTo(eyewalk)}>EyeWalk</button>
+        <button className='navLink' style={{ color: "#FFBB3F", fontWeight: "600" }} >Quero Experimentar</button>
+      </NavBar>
+      <Toolbar id="back-to-top-anchor" />
+      <section ref={inicio} className="inicioSection light">
+        Inicio
+      </section>
+      <section ref={quemSomos} className="quem-somosSection dark">
+        Quem somos
+      </section>
+      <section ref={planos} className="planosSection light">
+        Planos
+      </section>
+      <section ref={eyewalk} className="eyewalkSection dark">
+        EyeWalk
+      </section>
+      <ScrollTop />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,45 @@
+import Toolbar from '@mui/material/Toolbar';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+import logo from "../img/logoEyeWalk.png"
+
+const NavBar = (props) => {
+
+    return (
+        <AppBar position="fixed" sx={{ backgroundColor: "#192230" }}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 1,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: "Inter",
+                            fontWeight: 400,
+                            letterSpacing: '.1rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        EyeWalk
+                    </Typography>
+                    <img src={logo} alt='logo eyewalk' width="40" height="20" className='logo' />
+
+                    <Box sx={{ display: { xs: 'flex', md: 'flex' }, }}>
+                        {props.children}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar >
+    )
+
+}
+
+export default NavBar
